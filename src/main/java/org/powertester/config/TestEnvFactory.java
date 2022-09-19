666306122df0e92309b8fc4a261b4add46982ae6
pramodkumaryadav <pramodyadav027@gmail.com>
2022-09-19 12:25:16 +0200

    trial 2 to fix issues

--- src/main/java/org/powertester/config/TestEnvFactory.java
+++ src/main/java/org/powertester/config/TestEnvFactory.java
@@ -23,19 +23,24 @@ public class TestEnvFactory {
     }
 
     private Config setConfig() {
-        log.info("setConfig called");
-        // Standard config load behavior: https://github.com/lightbend/config#standard-behavior
-        config = ConfigFactory.load();
+        try{
+            log.info("setConfig called");
+            // Standard config load behavior: https://github.com/lightbend/config#standard-behavior
+            config = ConfigFactory.load();
 
-        TestEnv testEnv = config.getEnum(TestEnv.class, "TEST_ENV");
+            TestEnv testEnv = config.getEnum(TestEnv.class, "TEST_ENV");
 
-        String path = String.format("src/main/resources/%s", testEnv);
-        File testEnvDir = new File(String.valueOf(path));
-        for (File file : testEnvDir.listFiles()) {
-            Config childConfig = ConfigFactory.load(String.format("%s/%s", testEnv, file.getName()));
-            config = config.withFallback(childConfig);
+            String path = String.format("src/main/resources/%s", testEnv);
+            File testEnvDir = new File(String.valueOf(path));
+            for (File file : testEnvDir.listFiles()) {
+                Config childConfig = ConfigFactory.load(String.format("%s/%s", testEnv, file.getName()));
+                config = config.withFallback(childConfig);
+            }
+
+            return config;
+        }catch(Exception exception){
+            exception.printStackTrace();
+            throw new IllegalStateException("Could not parse config");
         }
-
-        return config;
     }
 }
