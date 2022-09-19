7a0c400ec214ce86f97baae61b4104e0e4d1c37d
pramodkumaryadav <pramodyadav027@gmail.com>
2022-09-19 11:58:05 +0200

    back to old state

--- src/main/java/org/powertester/config/TestEnvFactory.java
+++ src/main/java/org/powertester/config/TestEnvFactory.java
@@ -25,30 +25,19 @@ public class TestEnvFactory {
     }
 
     private Config setConfig() {
-        try{
+        log.info("setConfig called");
+        // Standard config load behavior: https://github.com/lightbend/config#standard-behavior
+        config = ConfigFactory.load();
 
-            log.info("setConfig called");
-            // Standard config load behavior: https://github.com/lightbend/config#standard-behavior
-            config = ConfigFactory.load();
+        TestEnv testEnv = config.getEnum(TestEnv.class, "TEST_ENV");
 
-            TestEnv testEnv = config.getEnum(TestEnv.class, "TEST_ENV");
-            System.out.println("TestEnv: " + testEnv);
-
-//            Path path = Paths.get("src", "main", "resources", String.valueOf(testEnv).toLowerCase());
-            String path = String.format("src/main/resources/%s", String.valueOf(testEnv).toLowerCase());
-            File testEnvDir = new File(String.valueOf(path));
-            System.out.println("directory path: " + testEnvDir.getAbsolutePath());
-            for (File file : testEnvDir.listFiles()) {
-//                Path envFilePath = Paths.get(String.valueOf(testEnv).toLowerCase(), file.getName());
-//                Config childConfig = ConfigFactory.load(String.valueOf(envFilePath));
-            Config childConfig = ConfigFactory.load(String.format("%s/%s", String.valueOf(testEnv).toLowerCase(), file.getName()));
-                config = config.withFallback(childConfig);
-            }
-
-            return config;
-        }catch(Exception exception){
-            exception.printStackTrace();
-            throw new IllegalStateException("not able to parse config properties");
+        String path = String.format("src/main/resources/%s", testEnv);
+        File testEnvDir = new File(String.valueOf(path));
+        for (File file : testEnvDir.listFiles()) {
+            Config childConfig = ConfigFactory.load(String.format("%s/%s", testEnv, file.getName()));
+            config = config.withFallback(childConfig);
         }
+
+        return config;
     }
 }
