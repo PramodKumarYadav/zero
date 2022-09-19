9c2906aed18ebc148d3070fcb46c870dfd994cf9
pramodkumaryadav <pramodyadav027@gmail.com>
2022-09-19 11:49:48 +0200

    testing another fix

--- src/main/java/org/powertester/config/TestEnvFactory.java
+++ src/main/java/org/powertester/config/TestEnvFactory.java
@@ -34,14 +34,14 @@ public class TestEnvFactory {
             TestEnv testEnv = config.getEnum(TestEnv.class, "TEST_ENV");
             System.out.println("TestEnv: " + testEnv);
 
-            Path path = Paths.get("src", "main", "resources", String.valueOf(testEnv).toLowerCase());
-//        String testEnvDirPath = String.format("src/main/resources/%s", testEnv);
+//            Path path = Paths.get("src", "main", "resources", String.valueOf(testEnv).toLowerCase());
+            String path = String.format("src/main/resources/%s", String.valueOf(testEnv).toLowerCase());
             File testEnvDir = new File(String.valueOf(path));
             System.out.println("directory path: " + testEnvDir.getAbsolutePath());
             for (File file : testEnvDir.listFiles()) {
-                Path envFilePath = Paths.get(String.valueOf(testEnv).toLowerCase(), file.getName());
-                Config childConfig = ConfigFactory.load(String.valueOf(envFilePath));
-//            Config childConfig = ConfigFactory.load(String.format("%s/%s", testEnv, file.getName()));
+//                Path envFilePath = Paths.get(String.valueOf(testEnv).toLowerCase(), file.getName());
+//                Config childConfig = ConfigFactory.load(String.valueOf(envFilePath));
+            Config childConfig = ConfigFactory.load(String.format("%s/%s", String.valueOf(testEnv).toLowerCase(), file.getName()));
                 config = config.withFallback(childConfig);
             }
 
