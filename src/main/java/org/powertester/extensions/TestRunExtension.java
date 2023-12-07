7a1be9c086c18c4100ede163c81f77b7c161b8b0
pramodkumaryadav <pramodyadav027@gmail.com>
2023-12-07 14:56:26 +0100

    added test run extension.

---
+++ src/main/java/org/powertester/extensions/TestRunExtension.java
@@ -0,0 +1,47 @@
+package org.powertester.extensions;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.powertester.utils.DateUtils.getDateAsString;
+
+@Slf4j
+public class TestRunExtension implements BeforeAllCallback, ExtensionContext.Store.CloseableResource {
+    AtomicBoolean isTestRunStarted = new AtomicBoolean(false);
+
+    Path testRunReportPath = Paths.get("target", "test-run-reports", getDateAsString("yyyy-MM-dd/HH-mm"));
+
+    @Override
+    public void beforeAll(ExtensionContext context) {
+        try {
+            if (isTestRunStarted.compareAndSet(false, true)) {
+                log.info("Run this section only once at the beginning of the whole test run.");
+                context.getRoot().getStore(ExtensionContext.Namespace.GLOBAL).put("TestRunExtension", this);
+
+                // Add your database connection pool setup here.
+                // DBConnectionPool.getInstance().setup();
+                
+                // Create test run repository here.
+                Files.createDirectories(testRunReportPath);
+            }
+        } catch (Exception e) {
+            log.error("Exception in TestRunExtension: {}", e);
+            log.error("⚠ Terminating Test run since tests depend on the setup done in TestRunExtension class");
+            System.exit(1);
+
+        }
+    }
+
+    @Override
+    public void close() {
+        log.info("Run this section only once at the end of the whole test run.");
+
+        // Close your database connection pool here.
+        // DBConnectionPool.getInstance().close();
+    }
+}
