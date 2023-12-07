e94a0cb64d72f1c5f7a4f698b6a566ec13740e54
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2023-12-07 14:57:34 +0100

    added test run extension. (#109)

---
+++ src/main/java/org/powertester/utils/DateUtils.java
@@ -0,0 +1,16 @@
+package org.powertester.utils;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+public class DateUtils {
+    private DateUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static String getDateAsString(String format) {
+        LocalDateTime now = LocalDateTime.now(ZoneOffset.UTC);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        return now.format(formatter);
+    }
+}
